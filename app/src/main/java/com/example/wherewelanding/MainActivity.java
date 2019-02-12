292000167569816e2cd9a3ef5e6f2e3916d5c2a1
Michal Pieszczyk <michalpieszczyk@gmail.com>
2019-02-12 22:09:30 +0100

    Updated: Random spot generator, array of places.

--- app/src/main/java/com/example/wherewelanding/MainActivity.java
+++ app/src/main/java/com/example/wherewelanding/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,28 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgLandingSpot;
     TextView txtLandingSpot;
 
-    public int ChangeLandingSpotImage()
-    {
+    //landingSpots tiltedTowers = new landingSpots("Tilted Towers", getResources().getIdentifier("@drawable/imgtiltedtowers", null, this.getPackageName()));
+    //landingSpots paradisePalms = new landingSpots("Paradise Palms", getResources().getIdentifier("@drawable/imgparadisepalms", null, this.getPackageName()));
+
+    public landingSpots landingSpotsArray[] = new landingSpots[2];
+
+
+    public int ChangeLandingSpotImage() {
         int i = getResources().getIdentifier("@drawable/imgtiltedtowers", null, this.getPackageName());
         return i;
+    }
+
+    public void AssignLandingSpotsToArray(landingSpots _landingSpotsArray[])
+    {
+        _landingSpotsArray[0] = new landingSpots("Tilted Towers", getResources().getIdentifier("@drawable/imgtiltedtowers", null, this.getPackageName()));
+        _landingSpotsArray[1] = new landingSpots("Paradise Palms", getResources().getIdentifier("@drawable/imgparadisepalms", null, this.getPackageName()));
+
+    }
+
+    public landingSpots ReturnRandomSpot(landingSpots _landingSpotsArray[])
+    {
+        int randomSpot = new Random().nextInt(_landingSpotsArray.length);
+        return _landingSpotsArray[randomSpot];
     }
 
     @Override
@@ -30,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         int imageResource = getResources().getIdentifier("@drawable/imgbattlebus", null, this.getPackageName());
         imgBattleBus.setImageResource(imageResource);
 
+        AssignLandingSpotsToArray(landingSpotsArray);
 
         imgLandingSpot = (ImageView) findViewById(R.id.imgLandingSpot);
         imgLandingSpot.setVisibility(View.GONE);
@@ -43,9 +63,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                imgLandingSpot.setImageResource(ChangeLandingSpotImage());
-                String landingSpot = "Tilted Towers";
-                txtLandingSpot.setText(landingSpot);
+                imgLandingSpot.setVisibility(View.GONE);
+                txtLandingSpot.setVisibility(View.GONE);
+                
+                landingSpots generatedSpot = ReturnRandomSpot(landingSpotsArray);
+                imgLandingSpot.setImageResource(generatedSpot.getSpotImage());
+                txtLandingSpot.setText(generatedSpot.getSpotName());
+
                 imgLandingSpot.setVisibility(View.VISIBLE);
                 txtLandingSpot.setVisibility(View.VISIBLE);
             }
